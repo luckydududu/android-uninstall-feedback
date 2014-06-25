0efae4d650bd8492a0a0c3ef6840e5ca8a7e19a0
J.O.B <zzljob@gmail.com>
2014-06-25 16:26:08 +0800

    commit

--- library/gen/com/yepstudio/android/library/feedback/uninstall/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.yepstudio.android.library.feedback.uninstall;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
